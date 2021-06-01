626b29b3d18adf79374444cabc995923b99ceb53
STEPHANE POLETTI <polettistephane@msn.com>
2021-06-01 11:52:06 +0200

    validates length

--- app/models/goat_ad.rb
+++ app/models/goat_ad.rb
@@ -2,4 +2,6 @@ class GoatAd < ApplicationRecord
   validates :name, :age, :address, :longitude, :latitude, :description,
             :price_per_day, :features, :medias, :start_available,
             :end_available, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :description, length: { minimum: 20 }
 end
