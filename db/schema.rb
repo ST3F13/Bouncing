07531705931516c1bc590c841c37b09dc7aa3a13
ST3F13 <81956829+ST3F13@users.noreply.github.com>
2021-06-02 15:27:54 +0200

    add cards to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_01_152138) do
-
 ActiveRecord::Schema.define(version: 2021_06_01_152803) do
 
   # These are extensions that must be enabled in order to support this database
@@ -55,8 +52,6 @@ ActiveRecord::Schema.define(version: 2021_06_01_152803) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,4 +66,5 @@ ActiveRecord::Schema.define(version: 2021_06_01_152803) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
