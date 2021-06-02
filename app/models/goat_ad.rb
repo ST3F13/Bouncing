ea68707ec1b65a525c47082caa4134bf9d5286a5
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-02 17:05:59 +0200

    edit

--- app/models/goat_ad.rb
+++ app/models/goat_ad.rb
@@ -1,6 +1,6 @@
 class GoatAd < ApplicationRecord
   has_one_attached :media
-  validates :name, :age, :address, :longitude, :latitude, :description,
+  validates :name, :age, :description,
             :price_per_day, :features, presence: true
   validates :name, length: { minimum: 2 }
   validates :description, length: { minimum: 20 }
