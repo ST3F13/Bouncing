f33a2dddd688ab0264dae4951da196f4ffcdb33c
clementine cheyrou <clemcheyrou@gmail.com>
2021-06-02 14:43:18 +0200

    homepage-map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @goat_ads = GoatAd.all
+
+     @markers = @goat_ads.geocoded.map do |goat_ad|
+      {
+        lat: goat_ad.latitude,
+        lng: goat_ad.longitude
+      }
+    end
   end
 end
