b2133ffbda125f866c925075a69516d92d90b8ff
ST3F13 <81956829+ST3F13@users.noreply.github.com>
2021-06-03 14:21:37 +0200

    5 pages features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def space_traveller
+    "space-traveller"
+  end
+
+  def fighter
+    "fighter"
+  end
+
+  def milker
+    "milker"
+  end
+
+  def sociable
+    "sociable"
+  end
+
+  def mower
+    "mower"
+  end
+
+  def features_collection
+    %w[fighter milker mower sociable space-traveller]
+  end
 end
